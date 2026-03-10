6702fbf357a98353a18eabb4f08f55fdb66790bf
Vanio Antunes do Livramento Junior <vanio.antunes.md@gmail.com>
2026-03-10 15:22:07 -0300

    chore(01-02): R CMD check --as-cran compliance fixes
    - Add @importFrom clubSandwich vcovCR to resolve 'not imported from' NOTE
    - Fix DESCRIPTION Title to use title case ('Metafor' not 'metafor')
    - Add mockery to Suggests to resolve undeclared dependency WARNING
    - Result: 0 errors, 0 warnings, 2 acceptable NOTEs (new submission + time)

--- meta3l/R/meta3l-package.R
+++ meta3l/R/meta3l-package.R
@@ -3,6 +3,7 @@
 #' @importFrom metafor escalc vcalc rma.mv robust transf.ilogit transf.iarcsin
 #' @importFrom readxl excel_sheets read_excel
 #' @importFrom stats as.formula model.matrix
+#' @importFrom clubSandwich vcovCR
 "_PACKAGE"
 
 utils::globalVariables(c("yi", "vi", "TE_id"))
